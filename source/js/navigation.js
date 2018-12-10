f33063c86dac72253adbbf344c6a92904a7ff775
Aleksei Arro <alexey.arro@waytostart.ru>
2018-12-10 14:33:59 +0300

    Images and navigation added

---
+++ source/js/navigation.js
@@ -0,0 +1,10 @@
+const pageHeader = document.querySelector('.page-header');
+const pageHeaderButton = pageHeader.querySelector('.page-header__button');
+const pageHeaderButtonTextClosed = pageHeader.querySelector('.page-header__button-text--closed');
+const pageHeaderButtonTextOpened = pageHeader.querySelector('.page-header__button-text--opened');
+
+pageHeaderButton.addEventListener('click', function (evt) {
+  pageHeader.classList.toggle('is-visible');
+  pageHeaderButton.classList.toggle('page-header__button--close');
+  pageHeaderButton.classList.toggle('page-header__button--open');
+});
