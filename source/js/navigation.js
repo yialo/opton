78f173a1d32ead70eae14ec990f1f3b8372f302a
Aleksei Arro <alexey.arro@waytostart.ru>
2018-12-10 18:19:35 +0300

    Promo slider added

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -3,6 +3,6 @@ const pageHeaderButton = pageHeader.querySelector('.page-header__button');
 const pageHeaderButtonTextClosed = pageHeader.querySelector('.page-header__button-text--closed');
 const pageHeaderButtonTextOpened = pageHeader.querySelector('.page-header__button-text--opened');
 
-pageHeaderButton.addEventListener('click', function (evt) {
+pageHeaderButton.addEventListener('click', function () {
   pageHeader.classList.toggle('is-visible');
 });
