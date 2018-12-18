cbc9f22e751e4abefcba93e73f0c513e45228afb
Aleksei Arro <alexey.arro@waytostart.ru>
2018-12-18 13:07:24 +0300

    Modal windows for medium viewport added

--- source/js/modal.js
+++ source/js/modal.js
@@ -2,6 +2,7 @@ const pageWithModal = document.querySelector('.modal');
 const modalOverlay = pageWithModal.querySelector('.modal__overlay');
 const modalOpenButton = pageWithModal.querySelector('.modal__open-button');
 const modalCloseButton = pageWithModal.querySelector('.modal__close-button');
+const modalCancelButton = pageWithModal.querySelector('.modal__cancel-button');
 
 modalOpenButton.addEventListener('click', function (evt) {
   if (!pageWithModal.classList.contains('is-modal-visible')) {
@@ -11,6 +12,12 @@ modalOpenButton.addEventListener('click', function (evt) {
 });
 
 modalCloseButton.addEventListener('click' , function (evt) {
+  if (pageWithModal.classList.contains('is-modal-visible')) {
+    pageWithModal.classList.remove('is-modal-visible');
+  }
+});
+
+modalCancelButton.addEventListener('click' , function (evt) {
   if (pageWithModal.classList.contains('is-modal-visible')) {
     pageWithModal.classList.remove('is-modal-visible');
   }
