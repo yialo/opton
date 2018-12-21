7e0866063c6e56fc24c60f7933ca5786237a3108
Aleksei Arro <alexey.arro@waytostart.ru>
2018-12-21 13:59:07 +0300

    Page-header adaptive changed accroding to design

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,15 +1,15 @@
-const dropdown = document.querySelector('.dropdown');
-const dropdownToggle = dropdown.querySelector('.dropdown__toggle');
-const dropdownTipClosed = dropdown.querySelector('.dropdown__tip--closed');
-const dropdownTipOpened = dropdown.querySelector('.dropdown__tip--opened');
+// const dropdown = document.querySelector('.dropdown');
+// const dropdownToggle = dropdown.querySelector('.dropdown__toggle');
+// const dropdownTipClosed = dropdown.querySelector('.dropdown__tip--closed');
+// const dropdownTipOpened = dropdown.querySelector('.dropdown__tip--opened');
 
-dropdownToggle.addEventListener('click', function () {
-  dropdown.classList.toggle('is-menu-visible');
-});
+// dropdownToggle.addEventListener('click', function () {
+//   dropdown.classList.toggle('is-menu-visible');
+// });
 
-window.addEventListener('keydown', function (evt) {
-  if (evt.keyCode === 27 && dropdown.classList.contains('is-menu-visible')) {
-    evt.preventDefault();
-    dropdown.classList.toggle('is-menu-visible');
-  }
-});
+// window.addEventListener('keydown', function (evt) {
+//   if (evt.keyCode === 27 && dropdown.classList.contains('is-menu-visible')) {
+//     evt.preventDefault();
+//     dropdown.classList.toggle('is-menu-visible');
+//   }
+// });
